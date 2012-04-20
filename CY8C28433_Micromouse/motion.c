ef8d219520aa2b7909dcf9e775a0ed998b1c6b2b
Kevin Yang <Kevin@Kevin-BeigeBox.(none)>
2012-04-20 01:04:20 -0700

    Working basic obstacle avoidance

--- CY8C28433_Micromouse/motion.c
+++ CY8C28433_Micromouse/motion.c
@@ -8,21 +8,7 @@ void Motion_Init(void)
 
 void Motion_Update(void)
 { 
-	int _leftSpeed = 255;
-	int _rightSpeed = 200;
-	
-	_leftSpeed -= (adcIRRight*2);
-	_rightSpeed -= (adcIRLeft*2);
-	
-	if (adcIRRight > adcIRLeft)
-	{
-		_leftSpeed -= (adcIRFront*2);
-	}
-	else
-	{
-		_rightSpeed -= (adcIRFront*2);
-	}
-	
-	Motor_Drive_Left(_leftSpeed);
-	Motor_Drive_Right(_rightSpeed);
+	int _drivePower = 255 - (adcIRFront);
+	Motor_Drive_Left(_drivePower);
+	Motor_Drive_Right(_drivePower);
 }
