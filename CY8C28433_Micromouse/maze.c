cdebc9b5ab0c2f0b00793974e9af27ea734d18b4
Kevin Yang <Kevin@Kevin-BeigeBox.(none)>
2012-05-15 21:59:52 -0700

    Maze flooding queue working with test harness

--- CY8C28433_Micromouse/maze.c
+++ CY8C28433_Micromouse/maze.c
@@ -1,9 +1,112 @@
 #include "maze.h"
 
 #pragma data:page0
-unsigned char mazeFloodQueue[128];
+char mazeFloodHeadPointer;
+char mazeFloodTailPointer;
+char mazeFloodCount;
+char mazeFloodQueue[MAZE_QUEUE_SIZE];
+
 #pragma data:page1  // Gets RAM page 1 all to itself
-unsigned char mazeFlags[256];
+char mazeFlags[MAZE_CELL_COUNT];
+// [3] fWest
+// [2] fSouth
+// [1] fEast
+// [0] fNorth
+
 #pragma data:page2  // Gets RAM page 2 all to itself
-unsigned char mazeRouting[256];
+char mazeRouting[MAZE_CELL_COUNT];
 #pragma data:data
+
+// Stores the cell to use as the goal when flooding
+Cell mazeGoalCell;
+
+void Maze_Init(void)
+{
+	// Reset the flooding queue
+	mazeFloodHeadPointer = 0;
+	mazeFloodTailPointer = 0;
+	mazeFloodCount = 0;
+}
+
+void Maze_AddWall(Cell c, Direction d)
+{
+	char x = cellGetColumn(c);
+	char y = cellGetRow(c);
+	
+	// Set wall flags for the current cell
+	cellSetFlags(c, d);
+	
+	if (d == NORTH)
+	{
+		if (y != (MAZE_HEIGHT - 1))  // Check if a cell exists above
+		{
+			c += MAZE_WIDTH;         // Address the row above
+			cellSetFlags(c, SOUTH);  // Set it's south wall
+		}
+	}
+	else if (d == SOUTH)
+	{
+		if (y != 0)  // Check if a cell exists below
+		{
+			c -= MAZE_WIDTH;         // Address the row above
+			cellSetFlags(c, NORTH);  // Set it's north wall
+		}
+	}
+	else if (d == EAST)
+	{
+		if (x != (MAZE_WIDTH - 1))   // Check if a cell exists to the right
+		{
+			c += 1;                  // Address the cell to the right
+			cellSetFlags(c, WEST);   // Set it's west wall
+		}
+	}
+	else if (d == WEST)
+	{
+		if (x != 0)                   // Check if a cell exists to the left
+		{
+			c -= 1;                  // Address the cell to the left
+			cellSetFlags(c, EAST);   // Set it's EAST wall
+		}
+	}
+	
+}
+
+void Maze_Enqueue(Cell c)
+{
+	if (mazeFloodCount != MAZE_QUEUE_SIZE)  // Check that queue is not already full
+	{
+		if (!cellIsInQueue(c))              // Check that the cell is not already in the queue
+		{
+			mazeFloodCount ++;               // Increment size counter
+			mazeFloodQueue[mazeFloodHeadPointer] = c;  // Put the cell index in the queue
+			
+			cellSetFlags(c, CELL_IN_QUEUE); // Set the cell's "in queue" flag
+			
+			mazeFloodHeadPointer ++;                             // advance head pointer
+			mazeFloodHeadPointer &= MAZE_QUEUE_MASK;             // mask off overflow
+		}
+	}
+}
+
+Cell Maze_Dequeue(void)
+{
+	if (mazeFloodCount)
+	{
+		Cell result;
+		
+		mazeFloodCount --;  // decrement size counter
+		
+		result = mazeFloodQueue[mazeFloodTailPointer];       // Retrieve the cell index at the tail pointer
+		mazeFlags[result] &= (~CELL_IN_QUEUE);               // Clear the cell's "in queue" flag
+		
+		mazeFloodTailPointer ++;                             // advance tail pointer
+		mazeFloodTailPointer &= MAZE_QUEUE_MASK;             // mask off overflow
+		
+		return result;
+	}
+	else
+	{
+		// The queue is empty
+		return 0;
+	}
+}
--- CY8C28433_Micromouse/maze.h
+++ CY8C28433_Micromouse/maze.h
@@ -6,4 +6,38 @@
 #include "PSoCGPIOINT.h"
 #include "MicromouseModules.h"
 
+#define MAZE_HEIGHT 16
+#define MAZE_WIDTH  16
+#define MAZE_CELL_COUNT (MAZE_HEIGHT * MAZE_WIDTH)
+
+#define MAZE_QUEUE_SIZE 128  // Power of two
+#define MAZE_QUEUE_MASK 0x7F
+
+#define CELL_START  0x00   // Start cell
+#define CELL_CENTER 0x77   // Center of a 16x16 maze
+
+#define WEST  0x08
+#define SOUTH 0x04
+#define EAST  0x02
+#define NORTH 0x01
+#define CELL_IN_QUEUE 0x10
+
+#define cellWallExists(c, d)   (mazeFlags[c] &  d)
+#define cellSetFlags(c, f)     (mazeFlags[c] |= f)
+
+#define cellIsInQueue(c)       (mazeFlags[c] & CELL_IN_QUEUE)
+
+#define cellGetColumn(c)       (c & 0x0F)
+#define cellGetRow(c)          (c >> 4)
+
+typedef char Cell;
+typedef char Direction;
+
+void Maze_Init(void);
+
+void Maze_AddWall(Cell c, Direction d);
+
+void Maze_Enqueue(Cell c);
+Cell Maze_Dequeue(void);
+
 #endif
